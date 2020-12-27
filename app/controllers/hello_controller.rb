494f8e2a0a14abc68c7959af0b64583843bab156
17021084 <dotrunghg1999@gmail.com>
2020-12-27 22:25:04 +0700

    add show method

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,8 @@ class HelloController < ApplicationController
   def index
     render json: "hello", status: :ok
   end
+
+  def show
+    render json: "hello #{params[:id]}", status: :ok
+  end
 end
